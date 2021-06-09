9d051afe83492c53a30d43e53c48ae8ae3385695
Desiree Zalm <82588366+desireezalm@users.noreply.github.com>
2021-06-09 16:52:23 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,21 @@
+const makePizza = function () {
+    console.log("Add flour, water and eggs to bowl")
+    console.log("Mix the ingredients")
+    console.log("Knead the dough")
+    console.log("Roll out the dough")
+    console.log("Put the pizza in the over and bake for 25 minutes")
+}
+
+const makeSushi = function () {
+    console.log("Cook the rice")
+    console.log("Cut the fish in slices")
+    console.log("Make a roll of the rice")
+    console.log("Add the slices of fish")
+    console.log("Wrap the rice and fish in a sheet of dried seawead")
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
